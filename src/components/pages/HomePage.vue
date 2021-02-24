0a0714c5396057f4c531070f2daf879d4497a162
santi <faizatulaisyah48@gmail.com>
2021-02-24 11:49:25 +0700

    baru

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,4 +1,4 @@
-(<template>
+<template>
   <v-card
     dark
     flat
@@ -99,6 +99,7 @@
                                 </v-card-actions>
                             </v-date-picker>
                         </v-card-text>
+                        
                     </template>
                 </v-dialog>
         </v-bottom-navigation>
@@ -162,6 +163,7 @@
                             <div v-show="show">
                                     <input  class="jadwal1 ml-5" v-model="senin1">
                                     <input  class="jadwal2 ml-5" v-model="senin2">
+                                    <input  class="jadwal3 ml-5" v-model="senin3">
                             </div>
                         </v-expand-transition>
 
@@ -177,6 +179,7 @@
                             <div v-show="show">
                                 <input  class="jadwal1 ml-5" v-model="selasa1">
                                 <input  class="jadwal2 ml-5" v-model="selasa2">
+                                <input  class="jadwal3 ml-5" v-model="selasa3">
                             </div>
                         </v-expand-transition>
                         
@@ -192,6 +195,7 @@
                             <div v-show="show">
                                 <input  class="jadwal1 ml-5" v-model="rabu1">
                                 <input  class="jadwal2 ml-5" v-model="rabu2">
+                                <input  class="jadwal3 ml-5" v-model="rabu3">
                             </div>
                         </v-expand-transition>
 
@@ -207,6 +211,7 @@
                             <div v-show="show">
                                 <input  class="jadwal1 ml-5" v-model="kamis1">
                                 <input  class="jadwal2 ml-5" v-model="kamis2">
+                                <input  class="jadwal3 ml-5" v-model="kamis3">
                             </div>
                         </v-expand-transition>
 
@@ -222,6 +227,7 @@
                             <div v-show="show">
                                 <input  class="jadwal1 ml-5" v-model="jumat1">
                                 <input  class="jadwal2 ml-5" v-model="jumat2">
+                                <input  class="jadwal3 ml-5" v-model="jumat3">
                             </div>
                         </v-expand-transition>
                         <v-col
@@ -265,12 +271,13 @@
                         class="elevation-1"
                     >
                     <template v-slot:item.hapus="{item}">
-                      
+                        
                             <v-btn icon @click="removeTodo(item)">
                             <v-icon color="pink">mdi-delete</v-icon>
                             </v-btn>    
                         
                     </template>
+                    
                     </v-data-table>
                 </div>
             </div>
@@ -349,14 +356,19 @@
         //jadwal
         senin1:'Isi jadwal 1',
         senin2:'Isi jadwal 2',
+        senin3:'Isi jadwal 3',
         selasa1:'Isi jadwal 1',
         selasa2:'Isi jadwal 2',
+        selasa3:'Isi jadwal 3',
         rabu1:'Isi jadwal 1',
         rabu2:'Isi jadwal 2',
+        rabu3:'Isi jadwal 3',
         kamis1:'Isi jadwal 1',
         kamis2:'Isi jadwal 2',
+        kamis3:'Isi jadwal 3',
         jumat1:'Isi jadwal 1',
         jumat2:'Isi jadwal 2',
+        jumat3:'Isi jadwal 3',
         jtmb:''
     }),
      methods: {
@@ -377,7 +389,7 @@
         },
 
         removeTodo(id) {
-        this.todos.splice(id, 1);
+        this.todos.splice(this.todos.indexOf(id), 1);
         this.simpan();
         },
 
@@ -386,17 +398,22 @@
         this.desk = "";
         },
 
-        simpanData() {
+    simpanData() {
       localStorage.senin1 = this.senin1;
       localStorage.senin2 = this.senin2;
+      localStorage.senin3 = this.senin3;
       localStorage.selasa1 = this.selasa1;
       localStorage.selasa2 = this.selasa2;
+      localStorage.selasa3 = this.selasa3;
       localStorage.rabu1 = this.rabu1;
       localStorage.rabu2 = this.rabu2;
+      localStorage.rabu3 = this.rabu3;
       localStorage.kamis1 = this.kamis1;
       localStorage.kamis2 = this.kamis2;
+      localStorage.kamis3 = this.kamis3;
       localStorage.jumat1 = this.jumat1;
       localStorage.jumat2 = this.jumat2;
+      localStorage.jumat3 = this.jumat3;
       localStorage.jtmb = this.jtmb;
       console.log('Diisi ya..');
     },
@@ -425,14 +442,19 @@
     //jadwal
     if(localStorage.senin1) this.senin1 = localStorage.senin1;
     if(localStorage.senin2) this.senin2 = localStorage.senin2;
+    if(localStorage.senin3) this.senin3 = localStorage.senin3;
     if(localStorage.selasa1) this.selasa1 = localStorage.selasa1;
     if(localStorage.selasa2) this.selasa2 = localStorage.selasa2;
+    if(localStorage.selasa3) this.selasa3 = localStorage.selasa3;
     if(localStorage.rabu1) this.rabu1 = localStorage.rabu1;
     if(localStorage.rabu2) this.rabu2 = localStorage.rabu2;
+    if(localStorage.rabu3) this.rabu3 = localStorage.rabu3;
     if(localStorage.kamis1) this.kamis1 = localStorage.kamis1;
     if(localStorage.kamis2) this.kamis2 = localStorage.kamis2;
+    if(localStorage.kamis3) this.kamis3 = localStorage.kamis3;
     if(localStorage.jumat1) this.jumat1 = localStorage.jumat1;
     if(localStorage.jumat2) this.jumat2 = localStorage.jumat2;
+    if(localStorage.jumat3) this.jumat3 = localStorage.jumat3;
     if(localStorage.jtmb) this.jtmb = localStorage.jtmb;
   },
   //catatan
@@ -472,6 +494,8 @@
     background-color: rgb(145, 211, 255);
     text-align: center;
 }
+.jadwal3{
+    background-color: rgb(145, 255, 200);
+    text-align: center;
+}
 </style>
-
-)
